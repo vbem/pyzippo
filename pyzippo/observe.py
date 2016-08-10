b2b9300aa83c6cd2ca1275c5dca910ea2b3f664c
vbem <i@lilei.tech>
2016-08-10 16:45:36 +0800

    add observation module

---
+++ pyzippo/observe.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+# coding: utf-8
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+r"""Utilities for code observation.
+"""
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+__all__     = []
+__version__ = (0, 1, 0, 'alpha', 0)
+__author__  = 'vbem <i@lilei.tech>'
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+import os
+import pprint
+import itertools
+import functools
+import logging
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# internal attributes
+_LOGGER = logging.getLogger(__name__)
+_LOGGER.addHandler(logging.NullHandler())
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# external attributes
+
+# valid logging levels
+LOG_LEVELS = (logging.DEBUG, logging.INFO, logging.WARNING, logging.ERROR, logging.FATAL)
+
+# demo logging format string
+# usage: `your_formatter = logging.Formatter(fmt=this_module.LOG_FMT)`
+LOG_FMT = '[%(asctime)s %(levelname)s %(name)s %(module)s,%(funcName)s,%(lineno)d] %(message)s'
+
+# demo logging formater
+# usage: `your_handler.setFormatter(this_module.LOG_FORMATTER)`
+LOG_FORMATTER = logging.Formatter(LOG_FMT)
+
+# demo logging handler to stderr
+# usage: `your_logger.addHandler(this_module.LOG_HANDLER_STDERR)`
+LOG_HANDLER_STDERR = logging.StreamHandler()
+LOG_HANDLER_STDERR.setFormatter(LOG_FORMATTER)
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+def colorize(sText, nFore=None, nBack=None, nAttr=None):
+    r"""Colorize text.
+    forecolors:  grey_30, red_31, green_32, yellow_33, blue_34, magenta_35, cyan_36, white_37.
+    backgrounds: grey_40, red_41, green_42, yellow_43, blue_44, magenta_45, cyan_46, white_47.
+    attributes:  bold_1, dark_2, underline_4, blink_5, reverse_7, concealed_8.
+    """
+    if not isinstance(sText, str):
+        raise TypeError('{sText!r} is not str type'.format(**locals()))
+    for nValue in (nFore, nBack, nAttr):
+        if not (nValue is None or isinstance(nValue, int)):
+            raise TypeError('argument value {nValue!r} is not valid'.format(**locals()))
+    if os.getenv('ANSI_COLORS_DISABLED') is None and any((nFore, nBack, nAttr)):
+        sFmtHead, sFmtTail = '\033[', 'm'
+        sFmt = ';'.join((str(nValue) for nValue in (nFore, nBack, nAttr) if nValue is not None))
+        return '{sFmtHead}{sFmt}{sFmtTail}{sText}{sFmtHead}{sFmtTail}'.format(**locals())
+    return sText
+
+def getPretty(*t, **d):
+    r"""Return "pretty-print" string for an object using `pprint.pformat()`.
+    Default `indent` is 4.
+    """
+    if 'indent' not in d and len(t) < 2:
+        d['indent'] = 4
+    return pprint.pformat(*t, **d)
+
+def reprArgs(*t, **d):
+    r'''Return arguments' representation string as in scrpit's function invocation.
+    '''
+    return '({})'.format(', '.join(itertools.chain( # chain generators, then join with comma, finally enclosed
+        (repr(arg) for arg in t),   # Generator of positional arguments
+        ('{}={!r}'.format(*tKv) for tKv in d.items()), # Generator of keyword arguments
+    )))
+
+def getFuncFullName(func):
+    r'''Return `package-name.module-name.class-name.method-name` like qualified name of given function object.
+    '''
+    if not callable(func):
+        raise TypeError('{func!r} is not callable'.format(**locals()))
+    return '{}.{}'.format(func.__module__, func.__qualname__)
+
+def atLog(logger=_LOGGER, nLevel=logging.DEBUG):
+    r'''A decorator wraps longging on function calls with given logger, level .
+    '''
+    if not isinstance(logger, logging.Logger):
+        raise TypeError('{logger!r} is not a logging.Logger instance'.format(**locals()))
+    if not isinstance(nLevel, int) or nLevel not in LOG_LEVELS:
+        raise ValueError('{nLevel!r} is not a valid logging level'.format(**locals()))
+    def decorator(func):
+        sFuncFullName = getFuncFullName(func)
+        @functools.wraps(func)
+        def wrapper(*t, **d):
+            sReprArgs = reprArgs(*t, **d)
+            logger.log(nLevel, '%s <= %s', sFuncFullName, sReprArgs)
+            result = func(*t, **d)
+            logger.log(nLevel, '%s => %r', sFuncFullName, result)
+            return result
+        return wrapper
+    return decorator
