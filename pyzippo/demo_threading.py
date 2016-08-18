e39083df4e4ab9fc437f43642c2d4600ac0d44f7
vbem <i@lilei.tech>
2016-08-18 21:26:37 +0800

    update threading

--- pyzippo/demo_threading.py
+++ pyzippo/demo_threading.py
@@ -17,11 +17,13 @@ import logging
 LOG = logging.getLogger(__name__)
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-class MultiThreads(list):
-    def __init__(self, nSize, **d):
+class ThreadsPool(list):
+    def __init__(self, nSize, clsThread=threading.Thread, **d):
         super().__init__() # empty list
         for n in range(nSize):
-            self.append(threading.Thread(name='{}-{}/{}'.format(d['target'].__name__, n+1, nSize), **d))
+            sNamePrefix = d['target'].__name__ if 'target' in d else clsThread.__name__
+            sName = '{}-{}/{}'.format(sNamePrefix, n+1, nSize)
+            self.append(clsThread(name=sName, **d))
 
     def start_all(self, *t, **d):
         for thread in self:
@@ -31,90 +33,108 @@ class MultiThreads(list):
         for thread in self:
             thread.join(*t, **d)
 
-class Crawler:
-    r'''Crawler multi-threading join based.
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+class ThreadUrls(threading.Thread):
+    r'''Thread generates URLs.
     '''
-    def __init__(self, iterUrls, nThreadsSize, nTimeout=None, *t, **d):
-        r"""Initialization.
-        """
-        super().__init__(*t, **d)
+    def __init__(self, iterUrls, qOutput, **d):
+        r'''
+        '''
+        super().__init__(**d)
         self._iterUrls = iterUrls
-        self._nThreadsSizeUrls = 1
-        self._nThreadsSizeResponses = nThreadsSize
-        self._nTimeoutResponses = nTimeout
+        self._qOutput = qOutput
 
-    def yieldResponses(self):
+    def run(self):
         r'''
         '''
-        # `_target_urls` output queue
-        self._qUrls = queue.Queue(2 * max((self._nThreadsSizeUrls, self._nThreadsSizeResponses)))
-
-        # `_target_responses` output queue
-        self._qResponses = queue.Queue(2 * self._nThreadsSizeResponses)
-
-        # `_target_urls` threads
-        self._threadsUrls = MultiThreads(nSize=self._nThreadsSizeUrls, target=self._target_urls)
-        self._threadsUrls.start_all()
-
-        # `_target_responses` threads
-        self._threadsResponses = MultiThreads(nSize=self._nThreadsSizeResponses, target=self._target_responses)
-        self._threadsResponses.start_all()
-
-        # `_target_sentinel` thread
-        self._threadSentinel = threading.Thread(target=self._target_sentinel, name='_target_sentinel')
-        self._threadSentinel.start()
-
-        # main thread
-        while True:
-            tResponse = self._qResponses.get()
-            LOG.debug('get {}'.format(tResponse))
-            if tResponse is EOFError:
-                self._threadSentinel.join() # wait `_target_sentinel` terminates
-                LOG.info('finish self._threadSentinel.join()')
-                break # terminate this thread
-            yield tResponse
-
-    def _target_urls(self):
-        r"""
-        """
         for tUrl in enumerate(self._iterUrls):
-            self._qUrls.put(tUrl) # may block
-            LOG.debug('{} put {}'.format(threading.current_thread().name, tUrl))
+            self._qOutput.put(tUrl) # may block
+            LOG.debug('%r put %r', threading.current_thread().name, tUrl)
         # terminate this thread
-        LOG.info('{} terminated'.format(threading.current_thread().name))
+        LOG.info('%r terminated', threading.current_thread().name)
 
-    def _target_responses(self):
+class ThreadResponses(threading.Thread):
+    r'''Thread generates responses.
+    '''
+    def __init__(self, qInput, qOutput, nTimeout=None, **d):
+        r'''
+        '''
+        super().__init__(**d)
+        self._qInput = qInput
+        self._qOutput = qOutput
+        self._nTimeout = nTimeout
+
+    def run(self):
         r'''
         '''
         while True:
-            tUrl = self._qUrls.get() # may block
-            LOG.debug('{} get {}'.format(threading.current_thread().name, tUrl))
+            tUrl = self._qInput.get() # may block
+            LOG.debug('%r get %r', threading.current_thread().name, tUrl)
             if tUrl is EOFError:
-                LOG.info('{} terminated'.format(threading.current_thread().name))
+                LOG.info('%r terminated', threading.current_thread().name)
                 break # terminate this thread
             nIndex, sUrl = tUrl
             try:
-                with urllib.request.urlopen(url=sUrl, timeout=self._nTimeoutResponses) as obj:
+                with urllib.request.urlopen(url=sUrl, timeout=self._nTimeout) as obj:
                     response = obj
             except Exception as e:
                 response = e
-            self._qResponses.put((nIndex, sUrl, response)) # may block
-            LOG.debug('{} put {}'.format(threading.current_thread().name, (nIndex, sUrl, response)))
+            self._qOutput.put((nIndex, sUrl, response)) # may block
+            LOG.debug('%r put %r', threading.current_thread().name, (nIndex, sUrl, response))
 
-    def _target_sentinel(self):
+class ThreadSentinel(threading.Thread):
+    r'''Thread sentinel.
+    '''
+    def __init__(self, qUrls, qResponses, poolUrls, poolResponses, **d):
         r'''
         '''
-        self._threadsUrls.join_all() # wait all `_target_urls` terminates
-        LOG.info('finish self._threadsUrls.join_all()')
-        for thread in self._threadsResponses:
-            self._qUrls.put(EOFError) # tell all `_target_responses` terminates
-        LOG.info('finish self._qUrls.put(EOFError)')
-        self._threadsResponses.join_all() # wait all `_target_responses` terminates
-        LOG.info('finish self._threadsResponses.join_all()')
-        self._qResponses.put(EOFError) # tell main thread terminates
-        LOG.info('finish self._qResponses.put(EOFError)')
+        d.setdefault('name', self.__class__.__name__)
+        super().__init__(**d)
+        self._qUrls = qUrls
+        self._qResponses = qResponses
+        self._poolUrls = poolUrls
+        self._poolResponses = poolResponses
+
+    def run(self):
+        r'''
+        '''
+        self._poolUrls.join_all()
+        LOG.info('joined all poolUrls')
+        for thread in self._poolResponses:
+            self._qUrls.put(EOFError)
+        LOG.info('told all poolResponses to terminate')
+        self._poolResponses.join_all()
+        LOG.info('joined all poolResponses')
+        self._qResponses.put(EOFError)
+        LOG.info('told main thread to terminate')
         # terminate this thread
-        LOG.info('{} terminated'.format(threading.current_thread().name))
+        LOG.info('%r terminated', threading.current_thread().name)
+        
+def yieldResponses(iterUrls, nConcurrentCount, nTimeout=None):
+    r'''
+    '''
+    qUrls = queue.Queue(2 * nConcurrentCount)
+    qResponses = queue.Queue(2 * nConcurrentCount)
+
+    poolUrls = ThreadsPool(nSize=1, clsThread=ThreadUrls, iterUrls=iterUrls, qOutput=qUrls)
+    poolUrls.start_all()
+
+    poolResponses = ThreadsPool(nSize=nConcurrentCount, clsThread=ThreadResponses, qInput=qUrls, qOutput=qResponses, nTimeout=nTimeout)
+    poolResponses.start_all()
+
+    threadSentinel = ThreadSentinel(qUrls=qUrls, qResponses=qResponses, poolUrls=poolUrls, poolResponses=poolResponses)
+    threadSentinel.start()
+
+    # main thread
+    while True:
+        tResponse = qResponses.get()
+        LOG.debug('main thread get %r', tResponse)
+        if tResponse is EOFError:
+            threadSentinel.join()
+            LOG.info('joined sentinel')
+            break # terminate this thread
+        yield tResponse
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 if __name__ == "__main__":
@@ -122,10 +142,8 @@ if __name__ == "__main__":
     LOG.addHandler(logging.NullHandler()) # as lib
     LOG.addHandler(journal.LOG_HANDLER_COLOR) # as app
 
-    LOG.setLevel(logging.DEBUG)
+    LOG.setLevel(logging.INFO)
 
-    c = Crawler(['http://www.liwanshi.com/']*30, nThreadsSize=5)
-    for tResponse in c.yieldResponses():
-        print(tResponse)
-
+    for nIndex, sUrl, response in yieldResponses(iterUrls=['http://www.liwanshi.com/']*1000, nConcurrentCount=100):
+        print(nIndex, sUrl, response)
 
